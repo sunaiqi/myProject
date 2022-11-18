29c14906e59b57910024d366c257fff06da082be
sunaiqi <1302914321@qq.com>
2022-11-18 14:27:21 +0800

    修改my_test文件里路径和params

--- api/my_test.py
+++ api/my_test.py
@@ -1,9 +1,54 @@
 import requests
 
-url = 'https://mddtest.wangli-tech.com:5443'
-header = {"Content-Type": "application/x-www-form-urlencoded", "client-version": "v1.0.8", "device-type": "0",
-          "user-type": "0"}
-data = {"phone": '15811289038', "vcode": '999999'}
+# url = 'https://mddtest.wangli-tech.com:5443'
+# header = {"Content-Type": "application/x-www-form-urlencoded", "client-version": "v1.0.8", "device-type": "0",
+#           "user-type": "0"}
+# data = {"phone": '15811289038', "vcode": '999999'}
+#
+#
+# def check_result(response, code=200):  # 检查结果
+#     assert response.status_code == 200
+#     assert response.json()['code'] == code
+#
+#
+# def get_url(path):  # 获取完整url
+#     return url + path
+#
+#
+# def login(phone='15811289038', vcode='999999'):  # 登录
+#     data['phone'] = phone
+#     data['vcode'] = vcode
+#     response = requests.post(url=get_url("/login/login"),
+#                              data=data,
+#                              headers=header)
+#     return response
+#
+#
+# def check_login():  # 检查是否登陆 未登陆情况登陆并设置到header
+#     if 'access-token' not in header:
+#         response = login()
+#         check_result(response)
+#         header['access-token'] = response.json()['data']['token']
+
+
+url = 'https://wlxltest.wangli-tech.com:24443'
+header = {
+          "ACCESS-TOKEN": "null",
+          "Accept": "*/*",
+          "Cache-Control": "no-cache",
+          "Connection": "keep-alive",
+          "Content-Length": "0",
+          "Origin": "https://testxspg.wangli-tech.com",
+          "Referer": "https://testxspg.wangli-tech.com/",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-site",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+          "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+          "userType": "3"
+}
+params = {"number": "13", "password": "13"}
+
 
 
 def check_result(response, code=200):  # 检查结果
@@ -15,13 +60,16 @@ def get_url(path):  # 获取完整url
     return url + path
 
 
-def login(phone='15811289038', vcode='999999'):  # 登录
-    data['phone'] = phone
-    data['vcode'] = vcode
-    response = requests.post(url=get_url("/login/login"),
-                             data=data,
+def login(number="13", password="13"):  # 登录
+    params['number'] = number
+    params['password'] = password
+    response = requests.post(url=get_url("/login/login/num-name?"),
+                             params=params,
                              headers=header)
     return response
+
+
+print(login())
 
 
 def check_login():  # 检查是否登陆 未登陆情况登陆并设置到header
@@ -29,3 +77,7 @@ def check_login():  # 检查是否登陆 未登陆情况登陆并设置到header
         response = login()
         check_result(response)
         header['access-token'] = response.json()['data']['token']
+
+
+
+
