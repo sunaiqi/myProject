f4041b2034ada51f31bab984ac94699d09e18082
sunaiqi <1302914321@qq.com>
2022-11-21 16:29:43 +0800

    1、删除无用文件 2、login文件新增断言

--- api/my_test.py
+++ api/my_test.py
@@ -1,35 +1,5 @@
 import requests
 
-# url = 'https://mddtest.wangli-tech.com:5443'
-# header = {"Content-Type": "application/x-www-form-urlencoded", "client-version": "v1.0.8", "device-type": "0",
-#           "user-type": "0"}
-# data = {"phone": '15811289038', "vcode": '999999'}
-#
-#
-# def check_result(response, code=200):  # 检查结果
-#     assert response.status_code == 200
-#     assert response.json()['code'] == code
-#
-#
-# def get_url(path):  # 获取完整url
-#     return url + path
-#
-#
-# def login(phone='15811289038', vcode='999999'):  # 登录
-#     data['phone'] = phone
-#     data['vcode'] = vcode
-#     response = requests.post(url=get_url("/login/login"),
-#                              data=data,
-#                              headers=header)
-#     return response
-#
-#
-# def check_login():  # 检查是否登陆 未登陆情况登陆并设置到header
-#     if 'access-token' not in header:
-#         response = login()
-#         check_result(response)
-#         header['access-token'] = response.json()['data']['token']
-
 
 url = 'https://wlxltest.wangli-tech.com:24443'
 header = {
@@ -50,7 +20,6 @@ header = {
 params = {"number": "13", "password": "13"}
 
 
-
 def check_result(response, code=200):  # 检查结果
     assert response.status_code == 200
     assert response.json()['code'] == code
@@ -60,7 +29,7 @@ def get_url(path):  # 获取完整url
     return url + path
 
 
-def login(number="13", password="13"):  # 登录
+def login(number="zywd1", password="zywd1"):  # 登录
     params['number'] = number
     params['password'] = password
     response = requests.post(url=get_url("/login/login/num-name?"),
@@ -69,7 +38,7 @@ def login(number="13", password="13"):  # 登录
     return response
 
 
-print(login())
+# print(login())
 
 
 def check_login():  # 检查是否登陆 未登陆情况登陆并设置到header
--- cases/test_login.py
+++ cases/test_login.py
@@ -1,72 +1,107 @@
-from api.my_test import *
+from myProject.api.my_test import *
 
 
-def test_agreement():  # 登录页查看用户协议和隐私政策
+def test_number_password_correct():  # 正确用户名和密码
     """
-    id = "41"
+    id = '1'
     """
-    response = requests.get(url="https://wmhtml.wangli-tech.com/PrivacyAgreement/index.html",
-                            headers=header)
-    assert "隐私政策" in response.content.decode("utf-8")
+    response = login()
+    assert response.status_code == 200
+    assert response.json()['code'] == 200
+    assert response.json()['data']['token']
 
-#正确用户名和密码
 
-def test_phone_empty():  # 手机号为空，返回200
+def test_number_empty():  # 用户名为空
     """
-    id = "17"
+    id = '2'
     """
-    response = login(phone='')
+    response = login(number="")
+    assert response.json() == {"msg": "该用户还未注册，请在中控端注册", "code": -1}
     assert response.status_code == 200
 
 
-def test_phone_err():  # 手机号不存在，返回400
+def test_password_empty():  # 密码为空
     """
-    id = "25"
+    id = "3"
     """
-    response = login(phone='11100000000')
-    assert response.json() == {'code': 400, 'msg': '该手机号还未注册！'}
+    response = login(password="")
+    assert response.json() == {"msg": "密码错误", "code": -2}
+    assert response.status_code == 200
 
 
-def test_patient_end():  # 疗程被终止
+def test_password_error():  # 密码错误
     """
-    id = "27"
+    id = "4"
     """
-    response = login(phone='13000000002')
-    assert response.json() == {'code': 400, 'msg': '您的主治医生已中止您的当前治疗，请与主治医生联系!'}
+    response = login(password="zywd11")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "密码错误", "code": -2}
 
 
-def test_vcode_err():  # 验证码错误，返回400
+def test_number_unregistered():  # 用户名未在中控注册
     """
-    id = "19"
+    id = "5"
     """
-    check_login()
-    response = login(vcode='123')
-    assert response.json() == {"msg": "验证码错误", "code": 400}
+    response = login(number="decade", password="decade")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "该用户还未注册，请在中控端注册", "code": -1}
+
+# def test_phone_empty():  # 手机号为空，返回200
+#     """
+#     id = "17"
+#     """
+#     response = login(phone='')
+#     assert response.status_code == 200
 
 
-def test_login_success():  # 登录成功，返回200
-    """
-    id = "15"
-    """
-    response = login()
-    check_result(response)
-
-
-def test_get_info():  # 登录成功后获取用户个人信息
-    """
-    id = "21"
-    """
-    check_login()
-    response = requests.get(get_url("/patient/currentPatientDetail"),
-                            headers=header)
-    check_result(response)
-
-
-def test_get_period():  # 获取用户疗程信息成功
-    """
-    id = "23"
-    """
-    check_login()
-    response = requests.get(url + "/patient/periodTimeDetail",
-                            headers=header)
-    check_result(response)
+# def test_phone_err():  # 手机号不存在，返回400
+#     """
+#     id = "25"
+#     """
+#     response = login(phone='11100000000')
+#     assert response.json() == {'code': 400, 'msg': '该手机号还未注册！'}
+#
+#
+# def test_patient_end():  # 疗程被终止
+#     """
+#     id = "27"
+#     """
+#     response = login(phone='13000000002')
+#     assert response.json() == {'code': 400, 'msg': '您的主治医生已中止您的当前治疗，请与主治医生联系!'}
+#
+#
+# def test_vcode_err():  # 验证码错误，返回400
+#     """
+#     id = "19"
+#     """
+#     check_login()
+#     response = login(vcode='123')
+#     assert response.json() == {"msg": "验证码错误", "code": 400}
+#
+#
+# def test_login_success():  # 登录成功，返回200
+#     """
+#     id = "15"
+#     """
+#     response = login()
+#     check_result(response)
+#
+#
+# def test_get_info():  # 登录成功后获取用户个人信息
+#     """
+#     id = "21"
+#     """
+#     check_login()
+#     response = requests.get(get_url("/patient/currentPatientDetail"),
+#                             headers=header)
+#     check_result(response)
+#
+#
+# def test_get_period():  # 获取用户疗程信息成功
+#     """
+#     id = "23"
+#     """
+#     check_login()
+#     response = requests.get(url + "/patient/periodTimeDetail",
+#                             headers=header)
+#     check_result(response)
